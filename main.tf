82dab4fd332aad7637839d2d5c249ea6f2c6653b
vicentepimer <46525000+vicentepimer@users.noreply.github.com>
2022-10-19 17:39:35 +0200

    Primeros cambios

--- main.tf
+++ main.tf
@@ -1,4 +1,19 @@
 # Comentario
 provider "aws" {
   region = "eu-west-3"
+  # secret_key = ""
+  # access_key = ""
+}
+
+# variable path clave ssh
+variable "ssh_key_path" {}
+
+
+# Recurso de clave SSH en AWS
+resource "aws_key_pair" "deployer" {
+ key_name = "deployer-key"
+ public_key = file(var.ssh_key_path)
+ tags = {
+  Name = "pepesan-ssh"
+ }
 }
